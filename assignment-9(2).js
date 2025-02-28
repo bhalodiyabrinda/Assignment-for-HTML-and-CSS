fc2ab5329346fd9b4b3b64879225a06b5d43a527
Brinda Bhalodiya <160879136+bhalodiyabrinda@users.noreply.github.com>
2025-02-28 22:32:39 +0530

    Add files via upload

---
+++ assignment-9(2).js
@@ -0,0 +1,14 @@
+function show()
+{
+    a=15;
+    b=09;
+    c=36;
+    b=04;
+
+    document.getElementById('a').innerHTML=a+'  ';
+    document.getElementById('b').innerHTML=b+'  ';
+    document.getElementById('c').innerHTML=c+'  ';
+    document.getElementById('d').innerHTML=d+'  ';
+    document.getElementById('e').innerHTML='CLICK';
+
+}
